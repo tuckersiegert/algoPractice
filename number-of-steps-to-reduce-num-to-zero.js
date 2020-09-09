70f3ff7380dbc212beafe880e950fb306b28b62f
Tucker <tuckersiegert@gmail.com>
2020-09-09 11:56:23 -0700

    added number-of-steps-to-reduce-num-to-zero.js

---
+++ number-of-steps-to-reduce-num-to-zero.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} num
+ * @return {number}
+ 
+ Given a non-negative integer num, return the number of steps to reduce it to zero. If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+ 
+ */
+var numberOfSteps  = function(num) {
+    
+    let count = 0;
+    
+    while (num > 0){
+        if (num % 2 === 1){
+            num -= 1;
+            count++;
+        }
+        else{
+            num /= 2;
+            count++;
+        }
+    }
+    
+    return count;
+    
+    
+    
+};
