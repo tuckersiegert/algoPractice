02de25868e34c4d84afc1061a7ea381d8ebde6c1
tuckersiegert <tuckersiegert@gmail.com>
2021-10-29 22:48:07 -0700

    Add files via upload
    added concatenationOfArray.js

---
+++ concatenationOfArray.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ * 
+ * Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,1]
+Output: [1,2,1,1,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+ * 
+ * 
+ */
+ let getConcatenation = function(nums) {
+    
+    let lengthOfArray = 2 * nums.length;
+    
+    let ans =  new Array(lengthOfArray);
+    
+    for (let i = 0; i < nums.length; i++ ){
+    
+        ans[i] = nums[i];
+        ans[i + nums.length] = nums[i];
+    }
+    
+    
+    return ans;
+    
+};
